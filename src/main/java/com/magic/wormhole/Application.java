b2f1da005c6d4399223ce2a33792b4a895b4b43b
Eimantas Lilia <eimantas.lilia@gmail.com>
2023-07-03 17:04:35 +0300

    Change app startup depending on CLI args

--- src/main/java/com/magic/wormhole/Application.java
+++ src/main/java/com/magic/wormhole/Application.java
@@ -2,12 +2,31 @@ package com.magic.wormhole;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import picocli.CommandLine;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.exit(SpringApplication.run(Application.class, args));
-	}
+    public static void main(String[] args) {
+        var commands = List.of("send", "recv");
 
+        Arrays.stream(args)
+                .filter(commands::contains)
+                .findAny()
+                .ifPresentOrElse(
+						arg -> runCommand(args),
+                        () -> runApp(args));
+    }
+
+    private static void runCommand(String[] args) {
+		System.out.println("Executing commands only...");
+		new CommandLine(new Wormhole()).execute(args);
+    }
+
+    private static void runApp(String[] args) {
+        SpringApplication.exit(SpringApplication.run(Application.class, args));
+    }
 }
