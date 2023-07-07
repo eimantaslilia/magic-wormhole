4835214c4e1d8b563b04e1bfb9cd89fb9b5d52eb
Eimantas Lilia <eimantas.lilia@gmail.com>
2023-07-07 11:11:05 +0300

    Return response entity from registrar client fetch

--- src/main/java/com/magic/wormhole/Registrar.java
+++ src/main/java/com/magic/wormhole/Registrar.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
 
 @RestController
 public class Registrar {
@@ -16,15 +17,16 @@ public class Registrar {
 
     @PostMapping(value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> register(HttpServletRequest request, @RequestBody RegistrationRequest registrationRequest) {
+        System.out.println("registering:" + registrationRequest);
         String hostname = request.getRemoteAddr();
         registry.register(registrationRequest, hostname);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @GetMapping("/fetch/{clientName}")
-    public ClientAddress getClientAddress(@PathVariable String clientName) {
+    public ResponseEntity<ClientAddress> getClientAddress(@PathVariable String clientName) {
         var address = registry.getAddress(clientName);
         System.out.println("Request made to fetch client by name: " + clientName + ", found: " + address);
-        return address;
+        return ResponseEntity.of(Optional.ofNullable(address));
     }
 }
--- src/main/java/com/magic/wormhole/RegistrarClient.java
+++ src/main/java/com/magic/wormhole/RegistrarClient.java
@@ -1,19 +1,18 @@
 package com.magic.wormhole;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import java.util.Optional;
 
 @Component
 public class RegistrarClient {
 
-    public Optional<ClientAddress> fetchClientAddress(String clientName) {
+    public ResponseEntity<ClientAddress> fetchClientAddress(String clientName) {
         return WebClient.create()
                 .get()
                 .uri("localhost:8080/fetch/" + clientName)
                 .retrieve()
-                .bodyToMono(ClientAddress.class)
-                .blockOptional();
+                .toEntity(ClientAddress.class)
+                .block();
     }
 }
